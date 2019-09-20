32eb611abb5fa56938338e60ffe40218aea35c6a
Gerald Tee <93geraldtee@gmail.com>
2019-09-20 16:28:45 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][:languages] = "C"
+
 end
 
 def adding_matz
@@ -80,7 +86,12 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
+
+return programmer_hash
 end
 
 def changing_alan
@@ -100,9 +111,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+     #alans_new_info = "GUI"
+
+programmer_hash[:alan_kay][:known_for] = "GUI"
+
+return programmer_hash
 end
 
 def adding_to_dennis
@@ -122,4 +135,8 @@ def adding_to_dennis
         }
      }
 
+_assembly = programmer_hash[:dennis_ritchie][:languages]
+_assembly[1] = "Assembly"
+
+return programmer_hash
 end
